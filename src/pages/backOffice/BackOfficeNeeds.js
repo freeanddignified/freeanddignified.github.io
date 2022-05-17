72452d0e78f40b671e7746379e45741ff96100f1
DemiurgeAsh <a.shaihaliev@gmail.com>
2022-05-17 12:43:44 +0300

    Update BackOfficeNeeds.js

--- src/pages/backOffice/BackOfficeNeeds.js
+++ src/pages/backOffice/BackOfficeNeeds.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 const BackOfficeNeeds = () => {
-  return <div>Blank text</div>;
+  return <div><form><input type="email"><br><br><input type="file">
+	<br><br>
+	<input type="submit">
+	</form></div>;
 };
 
 export default BackOfficeNeeds;
