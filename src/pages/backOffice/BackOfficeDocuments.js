0302971236da40857f608e9e0fd67e569be45709
DemiurgeAsh <a.shaihaliev@gmail.com>
2022-05-17 13:50:02 +0300

    Update BackOfficeDocuments.js

--- src/pages/backOffice/BackOfficeDocuments.js
+++ src/pages/backOffice/BackOfficeDocuments.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const BackOfficeDocuments = () => {
-  return <div>Blank text</div>;
+  return <div><frame>
+	<a href="#" title="Документ 1">Документ 1</a><br>
+	<a href="#" title="Документ 2">Документ 2</a><br>
+	<a href="#" title="Документ 3">Документ 3</a><br>
+	<a href="#" title="Документ 4">Документ 4</a><br>
+	<a href="#" title="Документ 5">Документ 5</a><br>
+	<a href="#" title="Документ 6">Документ 6</a><br>
+	<a href="#" title="Документ 7">Документ 7</a><br>
+	
+	</frame></div>;
 };
 
 export default BackOfficeDocuments;
