c73c2cd845fbe3d12ed1f949dee641e3db256ab3
DemiurgeAsh <a.shaihaliev@gmail.com>
2022-05-17 12:46:43 +0300

    Update BackOfficeNeeds.js

--- src/pages/backOffice/BackOfficeNeeds.js
+++ src/pages/backOffice/BackOfficeNeeds.js
@@ -1,10 +1,21 @@
 import React from "react";
 
-const BackOfficeNeeds = () => {
-  return <div><form><input type="email"><br><br><input type="file">
-	<br><br>
-	<input type="submit">
-	</form></div>;
+const BackOfficeDevelopment = () => {
+  return (
+      <Box
+          display="grid"
+          gridTemplateColumns="1fr"
+          gap={2}
+          sx={{ width: "100%", padding: "20px", backgroundColor: "#1976d2" }}
+      >
+          <Box display="flex" alignItems="center" justifyContent="center">
+              <iframe style={{width: "100%", height: "400px"}} src="https://www.youtube.com/embed/nIDdgrp5IXM" title="YouTube video player"
+                      frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen></iframe>
+          </Box>
+          
+      </Box>
+  )
 };
 
 export default BackOfficeNeeds;
